f62c4f0baf60dc7674026723c31f897155a1d447
Nadhil Auzan Oktaviandhi <nadhil_ao@bi.go.id>
2021-08-17 13:57:39 +0700

    Switched example

--- Layers/Geoms/geom_rug/example7.R
+++ Layers/Geoms/geom_rug/example7.R
@@ -2,5 +2,5 @@ p <- ggplot(mtcars, aes(wt, mpg)) +
   geom_point()
 p <-   
  p +
-  geom_rug(length = unit(0.05, "npc")) +
-  scale_y_continuous(expand = c(0.1, 0.1))
+  geom_rug(outside = TRUE) +
+  coord_cartesian(clip = "off")
